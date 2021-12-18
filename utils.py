406d29c4070b19f2c62e5745e73b7ba931be8294
Albly <alex.kronin777@gmail.com>
2021-12-18 15:16:02 +0300

    AWGN generator has been added

---
+++ utils.py
@@ -0,0 +1,29 @@
+import torch
+
+def toDb(value):
+    '''Transforms ratio to Decibels'''
+    return 10*torch.log10(value)
+
+
+def fromDb(value):
+    '''Transforms Decibels to ratio'''
+    return 10**(value/10.0)
+
+
+def get_noise(signal, SNR_dB):
+    '''
+    Generates Additive white Gaussian noise (AWGN) with the same shape
+    as shape of input @signal. Power of noise is calculsted by given
+    @SNR_db signal to noise ratio [dB].  
+    '''
+
+    P_signal = torch.mean(torch.abs(signal)**2) # calculate power of the signal [W]
+    D_noise = P_signal/fromDb(SNR_dB)           # calculate power of noise [W]
+    
+    mu = torch.zeros(1)                         # Mean of AWGN 
+    sigma = torch.sqrt(D_noise/2)               # Deviation of AWGN
+
+    noise = torch.normal(mean = mu, std = sigma, size = signal.shape) # Noise vector
+    return noise 
+
+    

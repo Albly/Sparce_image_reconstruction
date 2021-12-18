629681d4ab0694aadfc2b0be93cb932b8e7ea192
Albly <alex.kronin777@gmail.com>
2021-12-18 20:30:13 +0300

    loss

--- metrics.py
+++ metrics.py
@@ -2,27 +2,20 @@
 
 import torch
 
-def MSE(x_real,x_hat):
-    '''
-    Mean squared error metric in matrix form
-    Calculates MSE via Frobenius norm.  
-    '''
+# def MSE(x_real,x_hat):
+#     '''
+#     Mean squared error metric in matrix form
+#     Calculates MSE via Frobenius norm.  
+#     '''
 
-    #input shapes
-    x_real_shape = x_real.shape
-    x_hat_shape = x_hat.shape
+#     #input shapes
+#     x_real_shape = x_real.shape
+#     x_hat_shape = x_hat.shape
 
-    # input shapes must be equal
-    assert x_real_shape == x_hat_shape, 'Inputs should be the same size. got {0} and {1}'.format(x_real_shape, x_hat_shape)
+#     # input shapes must be equal
+#     assert x_real_shape == x_hat_shape, 'Inputs should be the same size. got {0} and {1}'.format(x_real_shape, x_hat_shape)
 
-    # if inputs are vectors
-    if len(x_real_shape) == 1:
-        # reshape them
-        x_real = x_real.reshape(1,-1)
-        x_hat = x_hat.reshape(1,-1)
-    # calculate MSE
-
-    return torch.norm(x_hat-x_real, 'fro')**2 / torch.numel(x_real)
+#     return torch.sum(torch.abs(x_real-x_hat)**2)/torch.numel(x_real)
 
 
 
@@ -30,5 +23,6 @@ def NMSE(x_real,x_hat):
     '''
     Normalized mean squared error in matrix form.
     '''
-    return MSE(x_real, x_hat) / MSE(x_real, torch.zeros_like(x_real))
+    mse = torch.nn.MSELoss()
+    return mse(x_real, x_hat) / mse(x_real, torch.zeros_like(x_real))
 

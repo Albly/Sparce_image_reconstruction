08b5214ab3db4e4e1fcded9bfaab6c1760823394
Albly <alex.kronin777@gmail.com>
2021-12-18 19:49:33 +0300

    MSE and NMSE metrics have been added

--- metrics.py
+++ metrics.py
@@ -1,5 +1,22 @@
 # This file contains functions with metrics that are going to be used for quality estimation
 
-def MSE(x,y):
-    pass 
+import torch
+
+def MSE(x_real,x_hat):
+    x_real_shape = x_real.shape
+    x_hat_shape = x_hat.shape
+
+    assert x_real_shape == x_hat_shape, 'Inputs should be the same size. got {0} and {1}'.format(x_real_shape, x_hat_shape)
+
+    if len(x_real_shape) == 1:
+        x_real = x_real.reshape(1,-1)
+        x_hat = x_hat.reshape(1,-1)
+    
+    return torch.norm(x_hat-x_hat, 'fro')**2 / torch.numel(x_real)
+
+
+
+def NMSE(x_real,x_hat):
+
+    return MSE(x_real, x_hat) / MSE(x_real, torch.zeros_like(x_real))
 

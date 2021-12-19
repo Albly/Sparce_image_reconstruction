61934b9fc5b7792c43dfda33ecb61904a3427fbf
Albly <alex.kronin777@gmail.com>
2021-12-19 14:22:04 +0300

    final version of noising function

--- utils.py
+++ utils.py
@@ -17,18 +17,10 @@ def get_noise(signal, SNR_dB):
     @SNR_db signal to noise ratio [dB].  
     '''
     
-    P_signal = torch.mean(torch.abs(signal)**2) # calculate power of the signal [W]
-    P_noise = P_signal*10**(-0.1*SNR_dB) # calculate power of noise [W]
-    D_noise = torch.sqrt(P_noise) # Deviation of AWGN
-    noise = D_noise*torch.randn_like(signal) # noise
+    P_signal = torch.mean(torch.abs(signal)**2)         # calculate power of the signal [W]
+    P_noise = P_signal*10**(-0.1*SNR_dB)                # calculate power of noise [W]
+    D_noise = torch.sqrt(P_noise)                       # Deviation of AWGN
+    noise = D_noise*torch.randn_like(signal)            # AWGN with zero mean and @D_noise std
 
-#     P_signal = torch.mean(torch.abs(signal)**2) # calculate power of the signal [W]
-#     D_noise = P_signal/fromDb(SNR_dB)           # calculate power of noise [W]
-    
-#     mu = torch.zeros(1)                         # Mean of AWGN 
-#     sigma = torch.sqrt(D_noise)               # Deviation of AWGN
-
-#     shape = signal.shape                        # get shape of image
-#     noise = torch.normal(mean = mu.repeat(shape), std = sigma.repeat(shape)) # Noise vector
     return noise
 

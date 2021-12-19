8797939a6969c786ccf59f186e69ba05ea783afb
Skrikunov <krikunov.stanis@gmail.com>
2021-12-19 12:52:58 +0300

    SSIM methic has been added

--- metrics.py
+++ metrics.py
@@ -2,14 +2,16 @@
 
 import torch
 from torch.nn.functional import mse_loss
+import numpy as np
+import cv2
 
 
 def NMSE(x_real,x_hat):
     '''
     Normalized mean squared error in matrix form.
     '''
-
-    return mse_loss(x_real, x_hat) / mse_loss(x_real, torch.zeros_like(x_real))
+    nmse = mse_loss(x_real, x_hat) / mse_loss(x_real, torch.zeros_like(x_real))
+    return nmse.item()
 
 
 def PSNR(x_real,x_hat):
@@ -22,5 +24,47 @@ def PSNR(x_real,x_hat):
     # maximum possible intensity for torch.uint8 dtype
     MAX_I = 255
     # calculate PSNR by definition
-    PSNR = 10*torch.log10(MAX_I**2 / mse(x_real,x_hat))
-    return PSNR
+    PSNR = 10*torch.log10(MAX_I**2 / mse)
+    return PSNR.item()
+
+
+
+
+def SSIM(img1, img2):
+    '''
+    Structural similarity index measure (SSIM) 
+    '''
+    # USEFUL LINKS
+    # https://cvnote.ddlee.cc/2019/09/12/psnr-ssim-python
+    # https://medium.com/srm-mic/all-about-structural-similarity-index-ssim-theory-code-in-pytorch-6551b455541e
+    # https://www.tensorflow.org/api_docs/python/tf/image/ssim?hl=ru
+    # L -dynamic pyxel range 
+    
+    img1 = np.array(img1, dtype=np.float64)
+    img2 = np.array(img2, dtype=np.float64)
+    
+    L,k1,k2 = 255,0.01,0.03
+    C1 = (k1 * L)**2
+    C2 = (k2 * L)**2
+    
+    # create kernel
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+    
+    # calculate mean values
+    muX = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]
+    muY = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    muX_sq,muY_sq = muX**2,muY**2 
+    muXY = muX * muY
+    
+    # calculate deviations
+    sigmaX_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - muX_sq
+    sigmaY_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - muY_sq
+    sigmaXY = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - muXY
+    
+    # calculate SSIM over xy plane
+    ssim_map = ((2 * muXY + C1) * (2 * sigmaXY + C2)) / ((muX_sq + muY_sq + C1) * (sigmaX_sq + sigmaY_sq + C2))
+    
+    # average results
+    ssim = ssim_map.mean()
+    return ssim
--- utils.py
+++ utils.py
@@ -16,14 +16,19 @@ def get_noise(signal, SNR_dB):
     as shape of input @signal. Power of noise is calculsted by given
     @SNR_db signal to noise ratio [dB].  
     '''
-
-    P_signal = torch.mean(torch.abs(signal)**2) # calculate power of the signal [W]
-    D_noise = P_signal/fromDb(SNR_dB)           # calculate power of noise [W]
     
-    mu = torch.zeros(1)                         # Mean of AWGN 
-    sigma = torch.sqrt(D_noise/2)               # Deviation of AWGN
+    P_signal = torch.mean(torch.abs(signal)**2) # calculate power of the signal [W]
+    P_noise = P_signal*10**(-0.1*SNR_dB) # calculate power of noise [W]
+    D_noise = torch.sqrt(P_noise) # Deviation of AWGN
+    noise = D_noise*torch.randn_like(signal) # noise
 
-    shape = signal.shape                        # get shape of image
-    noise = torch.normal(mean = mu.repeat(shape), std = sigma.repeat(shape)) # Noise vector
-    return noise 
+#     P_signal = torch.mean(torch.abs(signal)**2) # calculate power of the signal [W]
+#     D_noise = P_signal/fromDb(SNR_dB)           # calculate power of noise [W]
+    
+#     mu = torch.zeros(1)                         # Mean of AWGN 
+#     sigma = torch.sqrt(D_noise)               # Deviation of AWGN
+
+#     shape = signal.shape                        # get shape of image
+#     noise = torch.normal(mean = mu.repeat(shape), std = sigma.repeat(shape)) # Noise vector
+    return noise
 
